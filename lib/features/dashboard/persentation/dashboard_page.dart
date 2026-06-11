b2539d58b4f0cd732c9748eea96cd229a3ecbcbb
Firoz Mohammad <firozm613@gmail.com>
2026-06-11 15:16:41 +0530

    design: implement detailed opacity gradient steps for bottom nav background

--- lib/features/dashboard/persentation/dashboard_page.dart
+++ lib/features/dashboard/persentation/dashboard_page.dart
@@ -62,8 +62,12 @@ class DashboardPage extends StatelessWidget {
             gradient: LinearGradient(
               colors: [
                 Colors.white.withOpacity(0.0),
+                Colors.white.withOpacity(0.1),
                 Colors.white.withOpacity(0.2),
+                Colors.white.withOpacity(0.3),
+                Colors.white.withOpacity(0.4),
                 Colors.white.withOpacity(0.5),
+                Colors.white.withOpacity(0.7),
                 Colors.white,
               ],
               begin: Alignment.topCenter,
