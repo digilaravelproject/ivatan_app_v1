ad4050d609e6aecc0bb42830ad0b7c5a1e1ade94
Firoz Mohammad <firozm613@gmail.com>
2026-06-11 18:24:53 +0530

    fix: import AppUrls in cart_controller.dart

--- lib/features/product/persentation/controller/cart_controller.dart
+++ lib/features/product/persentation/controller/cart_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../core/network/api_services.dart';
+import '../../../../core/network/app_urls.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/widgets/custom_dialog.dart';
 import '../../../payment/presentation/controller/payment_controller.dart';
